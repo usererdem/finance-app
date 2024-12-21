5a259e0e7b51d982d8cfed809876f57bd76bf610
erdemelvan <erdemelvan35@gmail.com>
2024-12-21 14:19:17 +0300

    feat: complete clerk auth

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p>this is an authenticated route</p>;
+  return <UserButton afterSwitchSessionUrl='/' />;
 }
